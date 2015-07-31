112ee31ab67a0f4ae5879928d53aa998083d8a20
Sergej Jurečko <sergej.jurecko@wpn.tv>
2015-07-31 07:21:33 +0200

    enable logging for now

--- c_src/actordb_driver_nif.c
+++ c_src/actordb_driver_nif.c
@@ -1,7 +1,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-// #define _TESTDBG_ 1
+#define _TESTDBG_ 1
 #ifdef __linux__
 #define _GNU_SOURCE 1
 #include <sys/mman.h>
