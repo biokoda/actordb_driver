6f9f2568f17a37f907a0c1353d8766a4f4fbe32a
Sergej Jurečko <sergej.jurecko@wpn.tv>
2016-02-02 19:13:30 +0100

    disable robust mutexes

--- c_src/actordb_driver_nif.c
+++ c_src/actordb_driver_nif.c
@@ -1,7 +1,7 @@
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#define _TESTDBG_ 1
+// #define _TESTDBG_ 1
 #ifdef __linux__
 #define _GNU_SOURCE 1
 #include <sys/mman.h>
@@ -162,7 +162,7 @@ static void unlock_write_txn(int nEnv, char syncForce, char *commit)
 			mdb_txn_abort(wmdb->txn);
 		wmdb->txn = NULL;
 		wmdb->usageCount = 0;
-		
+
 		if (syncForce)
 			mdb_env_sync(wmdb->env,1);
 
